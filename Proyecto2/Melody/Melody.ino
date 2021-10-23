982efe01afc9f1fcbd56266324610368f90641fc
est19264 <est19264@uvg.edu.gt>
2021-10-22 18:26:58 -0600

    SONIDO FINAL

--- Proyecto2/Melody/Melody.ino
+++ Proyecto2/Melody/Melody.ino
@@ -16,38 +16,37 @@
 void tone_function(int mel[], int duration[], int quant);
 int input = 0;
 //Intro music
-int melody_intro[] = { NOTE_GS3, NOTE_A3,0, NOTE_G3, 0,0,NOTE_FS3, NOTE_F3, NOTE_E3,0,0, NOTE_A3, NOTE_DS3, 
-0,NOTE_C4, NOTE_A3, NOTE_DS3, NOTE_C3, NOTE_B3, NOTE_GS3, NOTE_A3, NOTE_G3,0, 0,NOTE_A3, NOTE_A3}; //26
+int melody_intro[] = { NOTE_B4, NOTE_B4, NOTE_B4, 0, NOTE_B4, NOTE_B4, NOTE_B4,NOTE_DS5, NOTE_B4, NOTE_B4, NOTE_A4, NOTE_G4, 
+NOTE_A4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, 0, NOTE_B4, NOTE_B4, NOTE_B4,NOTE_DS5, NOTE_B4, NOTE_B4, NOTE_A4, NOTE_G4, 
+NOTE_A4, NOTE_B4}; //28
 
 //Main menu melody
 int melody_menu[] = {NOTE_C4, NOTE_F4, NOTE_A4, NOTE_F4, NOTE_F4, 
-                NOTE_C4, NOTE_F4, NOTE_A4, NOTE_G4,
-                NOTE_A4, NOTE_B4, NOTE_A4, NOTE_G4, NOTE_F4, NOTE_E4, NOTE_F4}; //16
+                NOTE_C4, NOTE_F4, NOTE_A4, NOTE_G4}; //9
                 
 //Failed shot
-int melody_failed[] = {NOTE_G4, NOTE_C4}; //LOSE SOUND 2
+int melody_failed[] = {NOTE_DS8, NOTE_B0}; //LOSE SOUND 2
 
 
 //Winner 
 int melody_winner[] = {NOTE_B5, NOTE_C5, NOTE_D5, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_D5, NOTE_E5}; //8
 
 //Game over music
-int melody_over[] = {NOTE_DS5, NOTE_D5, NOTE_CS5}; //GAME OVER 3
+int melody_over[] = {NOTE_DS4, NOTE_D4, NOTE_CS4, NOTE_CS3}; //GAME OVER 3
 
 
 //No more bullets
-int melody_bull[] = {NOTE_D3}; //1
+int melody_bull[] = {NOTE_C8}; //1
 
 
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
 //Intro music
-int noteDurations_intro[] = {12,12,12,12,2,2,12,12,12,12,1,
-12,12,12,12,12,12,12,12,12,12,12,12,8, 8, 8};
+int noteDurations_intro[] = {6,6,6,2,6, 6,6,4,4,4,2,
+2,2,2,6,6,6,2,6,6,6,4,4,4, 2, 2, 2, 2};
 
 //Main menu melody
 int noteDurations_menu[] = {8,8,8,8,8,
-                       4,4,4,4,
-                       8,8,8,8,8,8,8};
+                       4,4,8,8};
                        
 //Failed shot
 int noteDurations_failed[] = {4, 2};
@@ -56,10 +55,10 @@ int noteDurations_failed[] = {4, 2};
 int noteDurations_winner[] = {4,4,4,4,4,4,4,4};
 
 //Game over music
-int noteDurations_over[] = {2, 2, 1};
+int noteDurations_over[] = {3, 3, 2, 1};
 
 //No more bullets
-int noteDurations_bull[] = {1};
+int noteDurations_bull[] = {8};
 
 void setup(){
   Serial.begin(9600); 
@@ -91,11 +90,11 @@ void loop() {
         input = Serial.read();
         //Serial.println(input);
         if (input == 49){
-          tone_function(melody_intro, noteDurations_intro, 26);
+          tone_function(melody_intro, noteDurations_intro, 28);
          
         }
         else if (input == 50){
-          tone_function(melody_menu, noteDurations_menu, 16);
+          tone_function(melody_menu, noteDurations_menu, 9);
          
         }
         else if (input == 51){
@@ -107,11 +106,7 @@ void loop() {
           
         }
         else if (input == 53){
-          tone_function(melody_winner, noteDurations_winner, 8);
-          
-        }
-        else if (input == 54){
-          tone_function(melody_over, noteDurations_over, 3);
+          tone_function(melody_over, noteDurations_over, 4);
           
         }
     } 
